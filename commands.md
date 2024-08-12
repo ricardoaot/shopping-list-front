90e3ca12b2f17d5180b75c444b269263dc9e5027
ricardoaot <ricardoaot@gmail.com>
2024-08-12 10:38:18 -0500

    Create and Edit features implementation in the main page.

---
+++ commands.md
@@ -0,0 +1,14 @@
+## Git commands
+git init
+git log
+git status
+
+### Rename current branch with main
+git branch -M main
+
+### Add remote repository
+git remote add origin https://github.com/ricardoaot/shopping-list-front
+
+### When there are initialization conflicts
+git pull origin main --allow-unrelated-histories
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@
 "use client";
 
 import { useContext, useEffect, useState } from 'react';
-
 import { useSocket } from '../context/SocketContext';
-
-
-import { Container, Typography, List, ListItem, ListItemText, IconButton, Checkbox, Button, Paper } from '@mui/material';
+import { Container, Typography, List, ListItem, ListItemText, IconButton, Checkbox, TextField, Button, Paper } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CheckIcon from '@mui/icons-material/Check';
 import UndoIcon from '@mui/icons-material/Undo';
+import CheckboxOutlineIcon from '@mui/icons-material/CheckBoxOutlineBlank';  
+
 
 interface Item {
   id: string;
@@ -19,40 +18,32 @@ interface Item {
 
 const Home: React.FC = () => {
   const [items, setItems] = useState<Item[]>([]);
-  
+  const [editingItem, setEditingItem] = useState<string | null>(null);
+  const [editName, setEditName] = useState<string>('');
+  const [newItemName, setNewItemName] = useState<string>('');
+
   const socket = useSocket();
 
   useEffect(() => {
     if (socket) {
-
       const fetchItems = async () => {
         const res = await fetch('http://localhost:5000/items');
         const data = await res.json();
         setItems(data);
       };
+
       console.log('Loading items...');
-
       fetchItems();
-      //socket.on('itemAdded', fetchItems);
-      socket.on('itemAdded', (item) => {
-        console.log('Evento itemAdded recibido:', item);  // Imprime el evento y los datos
-        fetchItems();
-      });
-
+      socket.on('itemAdded', fetchItems);
       socket.on('itemUpdated', fetchItems);
       socket.on('itemDeleted', fetchItems);
 
       return () => {
-        //socket.off('itemAdded', fetchItems);
-        socket.off('itemAdded', (item) => {
-          console.log('Evento itemAdded recibido:', item);  // Imprime el evento y los datos
-          fetchItems();
-        });
+        socket.off('itemAdded', fetchItems);
         socket.off('itemUpdated', fetchItems);
         socket.off('itemDeleted', fetchItems);
       };
     }
-
   }, [socket]);
 
   const handleDelete = async (id: string) => {
@@ -67,11 +58,61 @@ const Home: React.FC = () => {
     });
   };
 
+  const handleEditClick = (item: Item) => {
+    setEditingItem(item.id);
+    setEditName(item.name);
+  };
+
+  const handleSaveClick = async (id: string) => {
+    await fetch(`http://localhost:5000/items/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: editName }),
+    });
+    setEditingItem(null);
+  };
+
+  const handleAddItem = async () => {
+    if (newItemName.trim() === '') return;
+
+    const response = await fetch('http://localhost:5000/items', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: newItemName, purchased: false }),
+    });
+
+    if (response.ok) {
+      setNewItemName('');
+    }
+  };
+
   return (
     <Container component={Paper} sx={{ padding: 3, marginTop: 5 }}>
       <Typography variant="h4" gutterBottom>
         Shopping List
       </Typography>
+
+      <TextField
+        value={newItemName}
+        onChange={(e) => setNewItemName(e.target.value)}
+        onKeyDown={(e)=>{
+          if(e.key === "Enter"){
+            e.preventDefault();
+            handleAddItem()
+          }
+        }}
+        size="small"
+        label="Add new item"
+        sx={{ marginBottom: 2, width: '100%' }}
+      />
+      <Button
+        onClick={handleAddItem}
+        variant="contained"
+        sx={{ marginBottom: 2 }}
+      >
+        Add Item
+      </Button>
+
       <List>
         {items.map((item) => (
           <ListItem key={item.id} secondaryAction={
@@ -84,19 +125,32 @@ const Home: React.FC = () => {
             <Checkbox
               checked={item.purchased}
               onChange={() => handleTogglePurchased(item.id, item.purchased)}
-              icon={<UndoIcon />}
+              icon={<CheckboxOutlineIcon />}
               checkedIcon={<CheckIcon />}
               sx={{ marginRight: 2 }}
             />
-            <ListItemText
-              primary={item.name}
-              primaryTypographyProps={{ 
-                sx: { 
-                  //textDecoration: 'line-through' 
-                  textDecoration: item.purchased ? 'line-through' : 'none' 
-                } 
-              }}
-            />
+            {editingItem === item.id ? (
+              <>
+                <TextField
+                  value={editName}
+                  onChange={(e) => setEditName(e.target.value)}
+                  size="small"
+                  sx={{ marginRight: 2 }}
+                />
+                <Button onClick={() => handleSaveClick(item.id)} variant="contained">Save</Button>
+              </>
+            ) : (
+              <ListItemText
+                primary={item.name}
+                primaryTypographyProps={{ 
+                  sx: { 
+                    textDecoration: item.purchased ? 'line-through' : 'none' 
+                  } 
+                }}
+                onClick={() => handleEditClick(item)}
+                sx={{ cursor: 'pointer' }}
+              />
+            )}
           </ListItem>
         ))}
       </List>
